b06ec8ecd6d5137315dc13ece9d56b70e0702c55
chaosmak789@gmail.com <e8575f8ccf>
2019-06-23 19:21:36 +0500

    [CGG] (?) second task

---
+++ src/main/java/ru/mokeev/service/tasks/SecondTask.java
@@ -0,0 +1,130 @@
+package ru.mokeev.service.tasks;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SecondTask extends JPanel {
+
+    private final int pixelSize = 1;
+
+
+    private Graphics g;
+    private double A, B, C;
+    private int direction;
+
+
+    public SecondTask(int a, int b, int c) {
+
+//        setSize(800, 600);
+        if ((a < 0 && c >= 0) || (c < 0 && a >= 0)) {
+            this.direction = 1;
+        } else {
+            this.direction = -1;
+        }
+        this.A = Math.abs(a);
+        this.B = Math.abs(b);
+        this.C = Math.abs(c);
+
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        this.g = g;
+
+        draw(0, 0);
+    }
+
+
+    private double p() {
+        return B * B / 2;
+    }
+
+    private void draw(int x, int y) {
+        double p = p();
+
+        //  центр
+        double x0 = A / C;
+        double y0 = 0;
+        System.out.println(x0);
+        System.out.println(y0);
+
+        double Sh, Sv, Sd;
+        Sd = getSd(x, y, p);  // диагональ
+        Sv = getSv(x, y, p);  // горизонталь
+        Sh = getSh(x, y, p);  // вертикаль
+
+        plot(g, direction * x0, y0, Color.RED, 2, 2);
+        while (x + x0 < getWidth()) // пока полотно не кончится
+        {
+            if (Math.abs(Sh) - Math.abs(Sv) <= 0) {
+                x++;
+                if (Math.abs(Sd) - Math.abs(Sh) < 0)
+                    y++;
+
+            } else {
+                y++;
+                if (Math.abs(Sv) - Math.abs(Sd) > 0)
+                    x++;
+
+            }
+            plot(g, direction * (x + x0), y0 + y);
+            plot(g, direction * (x + x0), y0 - y);
+
+            Sd = getSd(x, y, p);
+            Sv = getSd(x, y, p);
+            Sh = getSh(x, y, p);
+        }
+    }
+
+    private double getSd(double x, double y, double p) {
+        return ((y + 1) * (y + 1)) - 2 * p * (x * A / C + 1);
+    }
+
+    private double getSv(double x, double y, double p) {
+        return ((y + 1) * (y + 1)) - 2 * p * x * A / C;
+    }
+
+    private double getSh(double x, double y, double p) {
+        return (y * y) - 2 * p * (x * A / C + 1);
+    }
+
+    private void putPixel(Graphics g, Double x, Double y) {
+        g.setColor(Color.BLACK);
+        g.drawOval(x.intValue(), y.intValue(), 1, 1);
+    }
+
+    private void putPixel(Graphics g, Double x, Double y, Color color, int sizeX, int sizeY) {
+        g.setColor(color);
+        g.drawOval(x.intValue(), y.intValue(), sizeX, sizeY);
+    }
+
+
+    private void plot(Graphics g, double x, double y) {
+        double w = (getWidth() - 1) / pixelSize;
+        double h = (getHeight() - 1) / pixelSize;
+        double maxX = (w - 1) / 2;
+        double maxY = (h - 1) / 2;
+
+        double borderX = getWidth() - ((2 * maxX + 1) * pixelSize + 1);
+        double borderY = getHeight() - ((2 * maxY + 1) * pixelSize + 1);
+        double left = (x + maxX) * pixelSize + borderX / 2;
+        double top = (y + maxY) * pixelSize + borderY / 2;
+        putPixel(g, left, top);
+    }
+
+
+    private void plot(Graphics g, double x, double y, Color color, int sizeX, int sizeY) {
+        double w = (getWidth() - 1) / pixelSize;
+        double h = (getHeight() - 1) / pixelSize;
+        double maxX = (w - 1) / 2;
+        double maxY = (h - 1) / 2;
+
+        double borderX = getWidth() - ((2 * maxX + 1) * pixelSize + 1);
+        double borderY = getHeight() - ((2 * maxY + 1) * pixelSize + 1);
+        double left = (x + maxX) * pixelSize + borderX / 2;
+        double top = (y + maxY) * pixelSize + borderY / 2;
+        putPixel(g, left, top, color,sizeX, sizeY);
+    }
+}
--- src/main/kotlin/ru/mokeev/HomeWorkStarerService.kt
+++ src/main/kotlin/ru/mokeev/HomeWorkStarerService.kt
@@ -7,20 +7,58 @@ import ru.mokeev.Constants.height
 import ru.mokeev.Constants.width
 import ru.mokeev.Constants.xMax
 import ru.mokeev.Constants.xMin
-import ru.mokeev.service.DrawingService
+import ru.mokeev.service.helpers.BresenhamPanel
+import ru.mokeev.service.tasks.FirstTask
+import ru.mokeev.service.tasks.SecondTask
 import java.awt.Dimension
+import javax.swing.JFrame
+import javax.swing.WindowConstants.EXIT_ON_CLOSE
+
 
 @Service
 class HomeWorkStarerService {
 
-    @EventListener(value = [ApplicationReadyEvent::class])
-    fun drawFirstHomeWork() {
-        val drawingService = DrawingService(xMin, xMax, width, height)
 
-        val frame = javax.swing.JFrame()
-        frame.contentPane.preferredSize = Dimension(width.toInt(), height.toInt())
-        frame.pack()
-        frame.isVisible = true
-        frame.contentPane.add(drawingService)
+    @EventListener(value = [ApplicationReadyEvent::class])
+    fun init() {
+        second()
+
+//        drawFirstHomeWork()
     }
+}
+
+fun second() {
+    val f = JFrame()
+    f.defaultCloseOperation = EXIT_ON_CLOSE
+    f.contentPane.preferredSize = Dimension(width.toInt(), height.toInt())
+    f.pack()
+    f.isVisible = true
+
+    f.contentPane.add(SecondTask(-2, 10, 1))
+
+    f.setLocationRelativeTo(null)
+
+}
+
+fun drawFirstHomeWork() {
+    val frame = javax.swing.JFrame()
+    frame.contentPane.preferredSize = Dimension(width.toInt(), height.toInt())
+    frame.pack()
+    frame.isVisible = true
+    frame.defaultCloseOperation = EXIT_ON_CLOSE
+    frame.contentPane.add(FirstTask(xMin, xMax, width, height))
+}
+
+fun testBres() {
+
+
+    val f = JFrame()
+    f.defaultCloseOperation = EXIT_ON_CLOSE
+    f.title = "Bresenham"
+
+    f.contentPane.add(BresenhamPanel())
+    f.pack()
+
+    f.setLocationRelativeTo(null)
+    f.isVisible = true
 }
